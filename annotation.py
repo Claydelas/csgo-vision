85b5d279ac83d0465e8c99f566bff6e19434c178
Martin Valchev <mv1u18@soton.ac.uk>
2021-10-19 00:10:59 +0300

    automatic labelled image extraction from videos

--- annotation.py
+++ annotation.py
@@ -1,36 +1,29 @@
 import numpy as np
 import cv2
 
-# Open sample
-img = cv2.imread("1.png")
+def interpolate_frame(img):
+    # Convert sample to HSV color space
+    imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # More lenient mask for bound box color
+    mask = cv2.inRange(imghsv, np.array([0, 67, 175]), np.array([12, 255, 255]))
+    # Replace original bbox with interpolated pixels
+    img = cv2.inpaint(img, mask, 2, cv2.INPAINT_TELEA)
 
-# Convert sample to HSV color space
-imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return img
 
-# (hMin = 4 , sMin = 87, vMin = 191), (hMax = 12 , sMax = 255, vMax = 255)
-lower = np.array([4, 87, 191])
-upper = np.array([12, 255, 255])
+def annotate_frame(img):
+    # Convert sample to HSV color space
+    imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# Create a range mask for the color of bounding boxes found in samples
-mask = cv2.inRange(imghsv, lower, upper)
+    # (hMin = 4 , sMin = 87, vMin = 191), (hMax = 12 , sMax = 255, vMax = 255)
+    # Create a range mask for the color of bounding boxes found in samples
+    mask = cv2.inRange(imghsv, np.array([4, 87, 191]), np.array([12, 255, 255]))
 
-# Retrieve all contours
-contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Retrieve all contours
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# Find the largest contour
-areas = [cv2.contourArea(c) for c in contours]
-bbox = contours[np.argmax(areas)]
+    # Find the largest contour
+    areas = [cv2.contourArea(c) for c in contours]
+    bbox = contours[np.argmax(areas)]
 
-# More lenient mask for bound box color
-mask = cv2.inRange(imghsv, np.array([0, 67, 175]), np.array([12, 255, 255]))
-
-# Replace original bbox with interpolated pixels
-interpolated = cv2.inpaint(img, mask, 2, cv2.INPAINT_TELEA)
-cv2.imshow("Interpolated", interpolated)
-
-# Draw the largest contour (the contour that best approximates the bounding box in the sample)
-x, y, w, h = cv2.boundingRect(bbox)
-cv2.rectangle(interpolated, (x, y), (x+w, y+h), (0, 255, 0), 2)
-cv2.imshow("Preview", interpolated)
-
-cv2.waitKey(0)
+    return (img, cv2.boundingRect(bbox))
--- vid.py
+++ vid.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import sys
+from annotation import annotate_frame
 
 
 def video_to_frames(vid):
@@ -12,3 +14,41 @@ def video_to_frames(vid):
             f'frames/{os.path.basename(vid).split(".")[0]}-{count}.jpg', image)
         count += 1
         success, image = stream.read()
+    return 0
+
+
+def video_to_annotated_frames(vid, obj_class):
+    count = 0
+    stream = cv2.VideoCapture(vid)
+    success, image = stream.read()
+    while success:
+        stream.set(cv2.CAP_PROP_POS_MSEC, (count*1000))
+        image, (x, y, w, h) = annotate_frame(image)
+        name = f'frames/{os.path.basename(vid).split(".")[0]}-{count}'
+        # Save video frame as image
+        cv2.imwrite(f'{name}.jpg', image)
+        # Save image annotation as YOLO format
+        with open(f'{name}.txt', 'w') as f:
+            f.write(f'{obj_class} {x} {y} {w} {h}')
+        count += 1
+        success, image = stream.read()
+    return 0
+
+
+def main():
+    if len(sys.argv) > 2:
+        try:
+            return video_to_annotated_frames(sys.argv[1], sys.argv[2])
+        except:
+            return "Failed to extract frames from video."
+    if len(sys.argv) > 1:
+        try:
+            return video_to_frames(sys.argv[1])
+        except:
+            return "Failed to extract frames from video."
+
+    return "Please supply a valid video path as an arg."
+
+
+if __name__ == '__main__':
+    sys.exit(main())
