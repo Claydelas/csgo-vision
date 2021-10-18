dfb8bff427175fae1b9cb0d679fa946e551b24de
Martin Valchev <mv1u18@soton.ac.uk>
2021-10-18 22:31:04 +0300

    replace original bbox with interpolated pixels

--- annotation.py
+++ annotation.py
@@ -21,9 +21,16 @@ contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 areas = [cv2.contourArea(c) for c in contours]
 bbox = contours[np.argmax(areas)]
 
+# More lenient mask for bound box color
+mask = cv2.inRange(imghsv, np.array([0, 67, 175]), np.array([12, 255, 255]))
+
+# Replace original bbox with interpolated pixels
+interpolated = cv2.inpaint(img, mask, 2, cv2.INPAINT_TELEA)
+cv2.imshow("Interpolated", interpolated)
+
 # Draw the largest contour (the contour that best approximates the bounding box in the sample)
 x, y, w, h = cv2.boundingRect(bbox)
-cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-cv2.imshow("sample", img)
+cv2.rectangle(interpolated, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Preview", interpolated)
 
 cv2.waitKey(0)
