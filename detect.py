d36c7f51059429162f389c3468d618ecd3443a4e
Martin Valchev <mv1u18@soton.ac.uk>
2021-11-28 22:40:11 +0200

    use trained model to detect live gameplay

--- detect.py
+++ detect.py
@@ -1,19 +1,29 @@
 import cv2
 import mss
 import numpy
-from model import detect
+import torch
+import atexit
+from pathlib import Path
+
+Path('out').mkdir(exist_ok=True)
+
+# This would need a second, body part segmentation pass,
+# to figure out the position of the player model's head
+def detect(model, img):
+    res = model(img)
+    # draw detection boxes
+    ims = res.render()
+    return ims[0]
 
 with mss.mss() as sct:
+    model = torch.hub.load('./yolov5', 'custom', path='./data/weights/best.pt', source='local')
+    # TODO: Replace VideoWriter in prod
+    # Instead of saving an annotated video, this should instead calculate the the offset of
+    # the bounding box in regards to the middle of the frame and adjust the mouse
+    vid_writer = cv2.VideoWriter('./out/detections.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (1920, 1080))
+    atexit.register(vid_writer.release)
     while True:
         # Get raw pixels from the screen, save it to a Numpy array
         img = numpy.array(sct.grab(sct.monitors[0]))
-
-        # Placeholder
-        detections = detect(img)
-
-        # Display the detections
-        cv2.imshow("OpenCV/Numpy normal", detections)
-
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            break
+        detections = detect(model, img)
+        vid_writer.write(detections)
--- model.py
+++
@@ -1,4 +0,0 @@
-# Placeholder function that returns the original image augmented with
-# bounding boxes around detected objects
-def detect(img):
-    return img
